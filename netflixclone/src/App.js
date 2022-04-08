f388a6577a6040ff0dc2bd7ac19e97d3b1a0575b
thexwool <allan.dsm95@gmail.com>
2022-04-08 17:40:59 -0300

    main

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from 'react'
+import Tmdb from './Tmdb'
 
 export default () => {
+    const loadAll = async () => {
+        // Pegando a lista TOTAL
+        let list = await Tmdb.getHomeList();
+        console.log(list);
+    }
+    
+    useEffect(() => {
+        loadAll()
+    }, [])
+    
     return (
+
         <div>
             Olá Mundo, CARALHO!
         </div>
