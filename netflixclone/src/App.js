560d5f62464288ebcfb1fb00e805e4560ab6aa87
thexwool <allan.dsm95@gmail.com>
2022-04-14 13:11:27 -0300

    main

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,21 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import './App.css'
 import Tmdb from './Tmdb'
+import MovieRow from './components/MovieRow'
 
 export default () => {
-    const loadAll = async () => {
-        // Pegando a lista TOTAL
-        let list = await Tmdb.getHomeList();
-        console.log(list);
-    }
-    
+
+    const [movieList, setMovieList] = useState([])
+
     useEffect(() => {
+        const loadAll = async () => {
+            // Pegando a lista TOTAL
+            let list = await Tmdb.getHomeList();
+            setMovieList(list)
+        }
+
         loadAll()
     }, [])
     
     return (
-
-        <div>
-            Olá Mundo, CARALHO!
+        
+        <div className="page">
+            <section className="lists">
+                {movieList.map((item, key) => (
+                    <MovieRow key={key} title={item.title} items={item.items}/>
+                ))}
+            </section>
         </div>
     )
 }
