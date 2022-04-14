d3d8d79e09b1cf2fd04b79ef5125efdca67121f3
thexwool <allan.dsm95@gmail.com>
2022-04-14 16:03:26 -0300

    main

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -2,16 +2,25 @@ import React, { useEffect, useState } from 'react'
 import './App.css'
 import Tmdb from './Tmdb'
 import MovieRow from './components/MovieRow'
+import FeaturedMovie from './components/FeaturedMovie'
 
 export default () => {
 
     const [movieList, setMovieList] = useState([])
+    const [featureData, setFeatureData] = useState(null)
 
     useEffect(() => {
         const loadAll = async () => {
             // Pegando a lista TOTAL
             let list = await Tmdb.getHomeList();
             setMovieList(list)
+
+            // Pegando o filme em destaque (Featured)
+            let originals = list.filter(i => i.slug === 'originals')
+            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+            let chosen = originals[0].items.results[randomChosen]
+            let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+            setFeatureData(chosenInfo)
         }
 
         loadAll()
@@ -20,6 +29,11 @@ export default () => {
     return (
         
         <div className="page">
+
+            {featureData && // se featureData existe faça abaixo
+                <FeaturedMovie item={featureData}/>
+            }
+
             <section className="lists">
                 {movieList.map((item, key) => (
                     <MovieRow key={key} title={item.title} items={item.items}/>
---
+++ netflixclone/src/components/FeaturedMovie.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './FeaturedMovie.css'
+
+export default ({item}) => {
+    return (
+        <div>
+            <section className="featured">
+                <div>{item.original_name}</div>
+            </section>
+        </div>
+    )
+}
